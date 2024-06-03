e6151f47d38ebd94d3a72cf68bd79e5278de615e
m1sk9 <me@m1sk9.dev>
2024-06-03 23:43:31 +0900

    feat: add translation data

---
+++ components/i18n/PulsateHome.ts
@@ -0,0 +1,39 @@
+export const translationData = {
+  ja: {
+    TAGLINE: '人々のつながりを "再定義" する次世代の分散型SNS',
+    BUTTON_GETTING_STARTED: 'はじめる',
+    BUTTON_SEARCH_SERVER: 'サーバーを探す',
+    BUTTON_MORE_INFO: 'もっと詳しく',
+    HEADING_2: 'つながりを "再定義" する',
+    HEADING_BODY:
+      'Pulsate は 高速で開発者・ユーザーに優しい次世代の分散型SNSです。\n 誰もが気軽に情報を共有しより公平でオープンなデジタル空間を提供します。',
+    FEATURE_HEADING_1: 'ユーザー中心の設計',
+    FEATURE_BODY_1:
+      'Pulsate は常にユーザー中心の設計を心かけています。\n UI/UX は非常にシンプルで、分散型SNSを使ったことがない人でも使えるようチュートリアルを用意しています。',
+    FEATURE_HEADING_2: 'Pulsate以外の人とも',
+    FEATURE_BODY_2:
+      'Pulsate は ActivityPub を実装している他のプラットフォームのサーバーとも繋がれます。\n 別のサーバーにいる友達と繋がるためだけにアカウントを複数持つ必要はありません。',
+    FEATURE_HEADING_3: 'いつまでもオープンソース',
+    FEATURE_BODY_3:
+      'Pulsate はオープンソースの精神に基づき、ソースコードをインターネットに公開しているため誰でも開発に参加することができます。'
+  },
+  en: {
+    TAGLINE:
+      'The next generation of decentralised social networking that "redefines" people\'s connections',
+    BUTTON_GETTING_STARTED: 'Getting started',
+    BUTTON_SEARCH_SERVER: 'Find a server',
+    BUTTON_MORE_INFO: 'Learn more',
+    HEADING_2: 'Redefining connections',
+    HEADING_BODY:
+      'Pulsate is a fast and developer/user-friendly next-generation decentralised social networking service.\n It provides a more fair and open digital space where everyone can easily share information.',
+    FEATURE_HEADING_1: 'User-centric design',
+    FEATURE_BODY_1:
+      'Pulsate always keeps user-centric design in mind.\n The UI/UX is very simple, and tutorials are available so that even people who have never used a decentralised SNS can use it.',
+    FEATURE_HEADING_2: 'Connect with people outside of Pulsate',
+    FEATURE_BODY_2:
+      'Pulsate can connect with servers of other platforms that implement ActivityPub.\n You do not need to have multiple accounts just to connect with friends on different servers.',
+    FEATURE_HEADING_3: 'Forever open source',
+    FEATURE_BODY_3:
+      'Based on the spirit of open source, Pulsate has published its source code on the internet, so anyone can participate in development.'
+  }
+};
